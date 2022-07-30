4be54ffa76c633f9b1a4ffb04c62ea6f5056ea95
dannykhuu2 <dannykhuu2@gmail.com>
2022-07-29 22:16:27 -0400

    update image container

--- night-view-app/src/components/ImageContainer.tsx
+++ night-view-app/src/components/ImageContainer.tsx
@@ -1,24 +1,23 @@
 import Image from 'react-bootstrap/Image';
 
-const ImageContainer = (images: any) => {
-    return (
-        <>
-            <div className="card-container">
-                <Image src={images.url} rounded={true}></Image>
-                <div className="information">
-                    <div className="row">
-                        <p className="description-header col-12">
-                            {images.title} - {images.date}
-                        </p>
-                    </div>
-                    <div className="row">
-                        <p className="description-body">
-                            {images.explanation}
-                        </p>
-                    </div>  
+const ImageContainer = ({ images } : any) => {
+    return (    
+        <div className="card-container">
+            <img src={images.url}></img>
+            <Image src={images.url} rounded={true} />
+            <div className="information">
+                <div className="row">
+                    <p className="description-header col-12">
+                        {images.title} - {images.date}
+                    </p>
                 </div>
+                <div className="row">
+                    <p className="description-body">
+                        {images.explanation}
+                    </p>
+                </div>  
             </div>
-        </>
+        </div>
     )
 }
 
