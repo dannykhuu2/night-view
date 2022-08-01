11f529083b240a75aaf03d1cbe970555fe43ad1e
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-01 17:08:04 -0400

    add types file for interfaces and global strings

---
+++ night-view-app/src/services/types.ts
@@ -0,0 +1,10 @@
+interface likedImage {
+    title: string
+    url: string
+    copyright: string
+    explanation: string
+    hdurl: string
+}
+
+export const API_GATEWAY_URL: string = "http://localhost:3333";
+export default likedImage;
