ca1a053fa205a4df378ebf8aa3bb46bf93faf344
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-03 20:36:21 -0400

    add UI for sign up and sign in forms

---
+++ night-view-app/src/components/Auth.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./Auth.css"
+
+interface AuthProps {
+    isSignin: boolean;
+}
+
+const Auth = ({ isSignin } : AuthProps) => {
+
+    const [showSignin, setSignin] = useState(isSignin);
+
+    if (showSignin === true) {
+        return (
+          <div className="Auth-form-container">
+            <form className="Auth-form">
+              <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Sign In</h3>
+                <div className="text-center">
+                  Not registered yet?{" "}
+                  <span className="link-primary" onClick={() => setSignin(false)}>
+                    Sign Up
+                  </span>
+                </div>
+                <div className="form-group mt-3">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    className="form-control mt-1"
+                    placeholder="Enter email"
+                  />
+                </div>
+                <div className="form-group mt-3">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control mt-1"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <div className="d-grid gap-2 mt-3">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )
+      }
+    
+      return (
+        <div className="Auth-form-container">
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Sign Up</h3>
+              <div className="text-center">
+                Already registered?{" "}
+                <span className="link-primary" onClick={() => setSignin(true)}>
+                  Sign In
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label>Full Name</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="e.g Jane Doe"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      )
+}
+
+export default Auth;
