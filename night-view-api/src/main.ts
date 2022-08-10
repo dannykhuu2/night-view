ab7010e84fed4b433211d94e94cc1c28739d0c43
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-09 22:16:39 -0400

    configure to allow for cookie usage and parsing

--- night-view-api/src/main.ts
+++ night-view-api/src/main.ts
@@ -1,13 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONT_END_URL
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+  app.use(cookieParser());
   await app.listen(3333);
 }
 bootstrap();
