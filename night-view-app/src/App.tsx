463e44ad0301c58c97a45c1140f2ed0fe80ac577
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-20 21:17:28 -0400

    add component to frontend router

--- night-view-app/src/App.tsx
+++ night-view-app/src/App.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header/Header';
+import LikesPage from './pages/HomePage/LikesPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/likes" element={<LikesPage />} />
           <Route path="/auth" element={<Auth showSignIn={true}/>} />
         </Routes>
       </BrowserRouter>
