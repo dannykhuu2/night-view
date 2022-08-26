bcd0d3a4f299af1d6a7688d577723fa4c2d59c02
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-26 18:51:12 -0400

    remove unnecesary properties from edit dto

--- night-view-api/src/user/dto/edit-user.dto.ts
+++ night-view-api/src/user/dto/edit-user.dto.ts
@@ -4,12 +4,4 @@ export class EditUserDto {
     @IsEmail()
     @IsOptional()
     email?: string
-
-    @IsString()
-    @IsOptional()
-    firstName?: string
-    
-    @IsString()
-    @IsOptional()
-    lastName?: string
 }
