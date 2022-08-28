0776dd8c26c0a5bfd6fe3d56a3724361f70a0672
dannykhuu2 <dannykhuu2@gmail.com>
2022-08-28 00:19:21 -0400

    add additional like feature functions and fixes

--- night-view-app/src/services/ImageService.ts
+++ night-view-app/src/services/ImageService.ts
@@ -1,22 +1,34 @@
 import axios from 'axios';
-import { LikedImage, API_GATEWAY_URL, AddLikedImage } from './types';
+import { LikedImage, API_GATEWAY_URL, ImageType} from './types';
 
 const API_GET_IMAGES = (count: number) => `${API_GATEWAY_URL}/images/api/${count}`;
-const API_GET_LIKED_IMAGES = (userId: number) => `${API_GATEWAY_URL}/images/likes/${userId}`;
-const API_POST_LIKED_IMAGE = (userId: number) => `${API_GATEWAY_URL}/images/${userId}`;
+const API_GET_LIKED_IMAGES = () => `${API_GATEWAY_URL}/images/likes`;
+const API_POST_LIKE_IMAGE = () => `${API_GATEWAY_URL}/images`;
+const API_DELETE_UNLIKE_IMAGE_ID = (imageId: number) => `${API_GATEWAY_URL}/images/byId/${imageId}`
+const API_DELETE_UNLIKE_IMAGE_URL = (encodedUrl: string) => `${API_GATEWAY_URL}/images/byUrl/${encodedUrl}`
 
-export const fetchImages = async (count: number) => {
-    const response = await axios.get(API_GET_IMAGES(count), {withCredentials: true});
+export const fetchImages = async (count: number): Promise<Array<ImageType>> => {
+    const response = await axios.get(API_GET_IMAGES(count), { withCredentials: true });
     return response.data;
 }
 
-export const fetchLikedImages = async (userId: number) => {
-    const response = await axios.get(API_GET_LIKED_IMAGES(userId), {withCredentials: true});
+export const fetchLikedImages = async () : Promise<Array<LikedImage>> => {
+    const response = await axios.get(API_GET_LIKED_IMAGES(), { withCredentials: true });
     return response.data;
 }
 
-export const saveLikedImage = async (userId: number, payload: AddLikedImage) => {
-    const response = axios.post(API_POST_LIKED_IMAGE(userId), {
+export const postLikeImage = async (payload: ImageType) => {
+    const response = axios.post(API_POST_LIKE_IMAGE(), {
         ...payload
-    }, {withCredentials: true});
+    }, { withCredentials: true });
+}
+
+export const postUnlikeImageWithId = async (imageId: number) => {
+    const response = axios.delete(API_DELETE_UNLIKE_IMAGE_ID(imageId), { withCredentials: true });
+}
+
+export const postUnlikeImageWithUrl = async (url: string) => {
+    const encodedUrl = encodeURIComponent(url)
+    console.log(encodedUrl);
+    const response = axios.delete(API_DELETE_UNLIKE_IMAGE_URL(encodedUrl), { withCredentials: true });
 }
